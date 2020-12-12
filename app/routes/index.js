97b87d5109e8092244bc62e0f4d648a091329f52
박승진 <park@bagseungjin-ui-MacBookAir.local>
2020-12-12 23:14:12 +0900

    slonik 으로 postgres 연결

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const slonik = require('slonik');
+
+const pool = slonik.createPool('postgresql://scott:scott@localhost:5432/scottdb')
 
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', (req, res) => {
+  console.log(req.body);
+  const post = req.body;
+  pool.query(slonik.sql`INSERT INTO post (title, content) VALUES (${post.title}, ${post.content})`)
+  res.redirect('/');
+})
 module.exports = router;
